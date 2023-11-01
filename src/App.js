91ce73bb411171fd3c17068ad5e1fe4561956bd1
lin-zhang <109013057+half-planet@users.noreply.github.com>
2023-11-01 14:03:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+//sIc/ App. js
+import { useEffect, useState } from "react" ;
+import "./App.css" ;
 function App() {
+const [users, setUsers] = useState ([]);
+useEffect (() => {
+fetch ( '/api/users')
+ .then ( (res) => res. json ())
+ .then ( (data) => setUsers (data));
+},[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users</h1>
+        {users.map((user) => (
+          <p key= {user.id} > {user.name}</p>
+        ))}
     </div>
   );
 }
-
 export default App;
