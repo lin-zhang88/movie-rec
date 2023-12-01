2ac6a3ac309763df6176d706b00ce564a3c17de5
John Doe <johndoe@example.com>
2023-12-01 12:26:24 -0600

    Website template changes to color

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ useEffect (() => {
 fetch ( '/api/users')
  .then ( (res) => res.json ())
  .then ( (data) => setUsers (data));
-},[]);
+},[]); //the section of ' user.name ' --> ' user.nam ' == results in no placeholder name appearing on website 
 
   return ( //Everything in this purple bracket is html, unless it is in curly braces--> Javascript embedded
     <div className="App">
         {users.map((user) => (
-          <p key= {user.id} > {user.name}</p> //Information embedded in curly braces is Javascript
-        ))}
+          <p key= {user.id} > {user.nam}</p> //Information embedded in curly braces is Javascript. There is a change in user.name
+        ))}                                  
+        <p>'Click here'</p>
     </div>
   );
 }
