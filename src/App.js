b4a55a37998ea21a2f0d6b6cb4938b4854ee96c0
Dee-Kamsi <dkobinwa@mail.lipscomb.edu>
2023-12-10 22:20:32 -0600

    linked everything together

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-//sIc/ App. js
-import { useEffect, useState } from "react" ;
-import "./App.css" ;
+//sIc/App.js
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import './App.css';
+
 function App() {
-const [users, setUsers] = useState ([]);
-useEffect (() => {
-fetch ( '/api/users')
-.then ( (res) => res.json ())
-.then ( (data) => setUsers (data));
-},[]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
 
   return (
     <div className="App">
       <h1>WElCOME TO JDCL</h1>
       <h2>Searching for the perfect film?</h2>
-      <link rel="stylesheet" href="app.css"/>
-        {users.map((user) => (
-          <p class= "welcome">Please click below to begin.</p>
-        ))}
-  
-         <a href="https://www.jdclrecommendations.com">Start here</a>
-         <hr/>
-         <img src= "https://imgur.com/a/L7xBExd" alt = "jdcl drawing"/>
-
-         
-
+      {users.map((user) => (
+        <p className="welcome" key={user.id}>
+          Please click below to begin.
+        </p>
+      ))}
+      <hr />
+      <Link to="/Users/missd/OneDrive/Documents/movie-rec/src/lin.js/staticPath">Get Started</Link>
+      <img src="https://i.imgur.com/xhXZ3xB.jpg" alt="jdcl drawing" />
     </div>
-   
   );
-
 }
+
 export default App;
